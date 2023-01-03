503b8daa1d3e4df73121a7344da9702efe6d5885
YoucefKL <101994063+YoucefKL@users.noreply.github.com>
2023-01-03 01:09:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pvwmak
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/e3xzth)
---
+++ index.ts
@@ -0,0 +1,145 @@
+import { Gantt, Selection } from '@syncfusion/ej2-gantt';
+import { projectNewData } from './data-source';
+
+/**
+ * Default Gantt sample
+ */
+
+Gantt.Inject(Selection);
+
+let gantt: Gantt = new Gantt({
+  dataSource: projectNewData,
+  height: '450px',
+  taskFields: {
+    id: 'TaskID',
+    name: 'TaskName',
+    startDate: 'StartDate',
+    endDate: 'EndDate',
+    duration: 'Duration',
+    progress: 'Progress',
+    dependency: 'Predecessor',
+    child: 'subtasks',
+  },
+  treeColumnIndex: 1,
+  columns: [
+    { field: 'TaskID', width: 80 },
+    { field: 'TaskName', headerText: 'Name', width: 250 },
+    { field: 'StartDate' },
+    { field: 'EndDate' },
+    { field: 'Duration' },
+    { field: 'Predecessor' },
+    { field: 'Progress' },
+  ],
+  labelSettings: {
+    leftLabel: 'TaskName',
+  },
+  projectStartDate: new Date('03/24/2019'),
+  projectEndDate: new Date('07/06/2019'),
+});
+gantt.appendTo('#DefaultFunctionalities');
+
+
+import { enableRipple } from '@syncfusion/ej2-base';
+enableRipple(true);
+
+/**
+ * Sample for PERT Chart
+ */
+
+import {
+    Diagram, NodeModel, DataBinding, DiagramElement, StackPanel, VerticalAlignment, randomId,
+    SnapConstraints, TextStyleModel, TextElement, HorizontalAlignment, DiagramTools,
+    HierarchicalTree, ComplexHierarchicalTree, ConnectorModel
+} from '@syncfusion/ej2-diagrams';
+import { DataManager } from '@syncfusion/ej2-data';
+import * as Data from './diagram-data.json';
+
+export interface DataInfo {
+    [key: string]: string;
+}
+
+Diagram.Inject(DataBinding, HierarchicalTree, ComplexHierarchicalTree);
+
+
+function getConnectorDefaults(connector: ConnectorModel): ConnectorModel {
+    connector.type = 'Straight';
+    connector.style.strokeColor = '#979797';
+    connector.targetDecorator.width = 10;
+    connector.targetDecorator.height = 10;
+    connector.targetDecorator.style = { fill: '#979797', strokeColor: '#979797' };
+    return connector;
+}
+
+//customization of the node template.
+function setNodeTemplate(node: NodeModel): StackPanel {
+    let table: StackPanel = new StackPanel();
+    table.id = randomId();
+    table.style.fill = '#0069d9';
+    table.orientation = 'Vertical';
+    let nameKey: string = 'id';
+    let stack: StackPanel = new StackPanel();
+    stack.id = randomId();
+    stack.children = [];
+    stack.height = 25;
+    stack.orientation = 'Horizontal';
+    stack.style.fill = 'white';
+    stack.horizontalAlignment = 'Stretch';
+    addRows(stack, node);
+    table.children = [(getTextElement((node.data as DataInfo)[nameKey], 'Stretch', 170, 'Stretch')), stack];
+    (table.children[0].style as TextStyleModel).color = 'white';
+    (table.children[0].style as TextStyleModel).fontSize = 14;
+    return table;
+}
+
+function getTextElement(
+    text: string, alignment: HorizontalAlignment,
+    width?: number, valignment?: VerticalAlignment
+): DiagramElement {
+    let textElement: TextElement = new TextElement();
+    textElement.id = randomId();
+    textElement.content = text;
+    textElement.width = width;
+    textElement.height = 25;
+    textElement.horizontalAlignment = alignment;
+    textElement.verticalAlignment = valignment;
+    textElement.style.strokeWidth = 1;
+    textElement.style.strokeColor = '#b5b5b5';
+    textElement.style.fill = 'transparent';
+    textElement.style.color = '#3c3c3c';
+    textElement.relativeMode = 'Object';
+    return textElement;
+}
+
+function addRows(column: StackPanel, node: NodeModel): void {
+    let nodeInfo: DataInfo = node.data as DataInfo;
+    column.children.push(getTextElement(nodeInfo.startDate, 'Left', 70));
+    column.children.push(getTextElement(nodeInfo.duration, 'Center', 30));
+    column.children.push(getTextElement(nodeInfo.endDate, 'Right', 70));
+}
+
+
+    
+    //Initializes diagram control
+    let diagram: Diagram = new Diagram({
+        width: '100%', height: '499px', snapSettings: { constraints: SnapConstraints.None },
+        dataSourceSettings: {
+            id: 'id', parentId: 'Category',
+            dataSource: new DataManager((Data as any).pertChartData),
+            //binds the external data with node
+            doBinding: (nodeModel: NodeModel) => {
+                /* tslint:disable:no-string-literal */
+                nodeModel['shape'] = { type: 'Text' };
+            }
+        },
+        layout: {
+            type: 'ComplexHierarchicalTree', orientation: 'LeftToRight',
+            verticalSpacing: 100, horizontalSpacing: 70
+        },
+        //Sets the default values of connector
+        getConnectorDefaults: getConnectorDefaults,
+        //used to customize template of the node.
+        setNodeTemplate: setNodeTemplate,
+        tool: DiagramTools.ZoomPan
+    });
+    diagram.appendTo('#diagram');
+    diagram.fitToPage();
